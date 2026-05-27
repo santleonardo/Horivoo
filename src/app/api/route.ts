48be3a3fa135bbcc8b1990b3dfb0fa5f28d73d6d
santleonardo <54089176+santleonardo@users.noreply.github.com>
2026-05-27 11:00:26 -0300

    Delete src directory

--- src/app/api/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ app: "Horivoo", version: "1.0.0" });
-}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AppShell } from "@/components/horivoo/AppShell";
-
-export default function Home() {
-  return <AppShell />;
-}
