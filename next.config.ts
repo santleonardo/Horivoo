5f4bac37f175e9f20a063c79e0df4ea4f296f2eb
santleonardo <54089176+santleonardo@users.noreply.github.com>
2026-05-27 23:13:59 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
